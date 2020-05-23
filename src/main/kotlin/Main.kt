7e471b705c0315337b7f35d6d4d2f515314e519a
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-05-23 23:16:36 +0200

    prints exception

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,19 @@
+import kotlin.Exception
+
 class Main {
     fun runLambda(){
-        val projects = readProjects()
-        for(project in projects){
-            val latestChange = checkChangelog(project.changelog)
-            if(latestChange != project.lastRecordedChangelog){
-                project.lastRecordedChangelog = latestChange
-                tweet(project)
-                updateProjectLastRecordedChange(project)
+        try {
+            val projects = readProjects()
+            for (project in projects) {
+                val latestChange = checkChangelog(project.changelog)
+                if (latestChange != project.lastRecordedChangelog) {
+                    project.lastRecordedChangelog = latestChange
+                    tweet(project)
+                    updateProjectLastRecordedChange(project)
+                }
             }
+        }catch (e: Exception){
+            println("Exception:  ${e.message}")
         }
     }
 }
